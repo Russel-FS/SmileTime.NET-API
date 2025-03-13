09be260460aaf9c08088a2ec28b0a14d2ad5cdef
Russel-FS <russelfloressolano900@gmail.com>
2025-03-13 21:33:40 +0000

    actualizar origen permitido en la configuración de CORS

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", builder =>
     {
-        builder.WithOrigins("https://symmetrical-space-palm-tree-jj7vpv6r467x25p74-4200.app.github.dev") // Origen permitido
+        builder.WithOrigins("https://supreme-zebra-g4rjvj9x7qp5c9vwg-4200.app.github.dev") // Origen permitido
                .AllowAnyHeader()                   // Permitir cualquier header
                .AllowAnyMethod()                   // Permitir cualquier método HTTP
                .AllowCredentials();                // Permitir envío de cookies-autenticación
