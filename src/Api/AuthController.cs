b7651cc7b2f702e45c25b6b38cfc666ea3ac12df
Russel-FS <russelfloressolano900@gmail.com>
2025-03-18 08:28:52 +0000

    Agregar documentación XML a métodos en AuthController para mejorar la claridad y comprensión del código

--- src/Api/AuthController.cs
+++ src/Api/AuthController.cs
@@ -21,12 +21,24 @@ namespace SmileTimeNET_API.rest
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IConfiguration _configuration;
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="AuthController"/>.
+        /// </summary>
+        /// <param name="userManager">El administrador de usuarios.</param>
+        /// <param name="configuration"> La configuración de la aplicación.</param>
         public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// Realiza el inicio de sesión de un usuario.
+        /// </summary>
+        /// <param name="model">El modelo de inicio de sesión.</param>
+        /// <returns>Un objeto que contiene el token JWT, el email, el ID del usuario y la fecha de expiración del token.</returns>
+        /// <response code="200">El inicio de sesión fue exitoso.</response>
+        /// <response code="400">El usuario no existe o la contraseña es incorrecta.</response>
         [HttpPost("login")]
         [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
@@ -58,6 +70,13 @@ namespace SmileTimeNET_API.rest
             };
             return Ok(response);
         }
+        /// <summary>
+        /// Registra un nuevo usuario en el sistema.
+        /// </summary>
+        /// <param name="model">El modelo de registro que contiene el email y la contraseña del usuario.</param>
+        /// <returns>Una respuesta que indica si el registro fue exitoso o un error si no lo fue.</returns>
+        /// <response code="200">El registro fue exitoso y retorna un token JWT.</response>
+        /// <response code="400">El email ya está registrado o hubo errores al crear el usuario.</response>
         [HttpPost("register")]
         [AllowAnonymous]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
@@ -80,7 +99,6 @@ namespace SmileTimeNET_API.rest
 
             if (result.Succeeded)
             {
-
                 var tokenExpiration = DateTime.Now.AddDays(60);  // 60 días de expiración
                 var token = GenerateJwtToken(user, tokenExpiration);// Generar token JWT para login automático
 
@@ -97,7 +115,12 @@ namespace SmileTimeNET_API.rest
             return BadRequest(result.Errors);
         }
 
-        // genera un token JWT con los datos del usuario y lo firma con la clave secreta
+        /// <summary>
+        /// Genera un token JWT para un usuario específico con una fecha de expiración determinada. 
+        /// </summary>
+        /// <param name="user">El usuario para el cual se genera el token.</param>
+        /// <param name="expires">La fecha de expiración del token.</param>
+        /// <returns>El token JWT generado para el usuario .</returns> 
         private string GenerateJwtToken(ApplicationUser user, DateTime? expires)
         {
 
