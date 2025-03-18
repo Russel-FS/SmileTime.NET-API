6fd342de2c6e30d01368f3533f81c528f459636f
Russel-FS <russelfloressolano900@gmail.com>
2025-03-18 20:10:00 +0000

    Agregar interfaz IAuthService y actualizar AuthResponse para incluir roles de usuario

---
+++ src/Domain/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SmileTimeNET_API.Models;
+
+
+namespace SmileTimeNET_API.src.Domain.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> LoginAsync(LoginModel model);
+        Task<AuthResponse> RegisterAsync(RegisterModel model);
+        Task<string> GenerateJwtTokenAsync(ApplicationUser user, DateTime expires);
+    }
+}
