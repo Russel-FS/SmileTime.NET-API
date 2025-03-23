aa069b3bb70a64372a21139dfb0066f5336ea67d
FrsDieOne <russelfloressolano900@outlook.es>
2025-03-23 00:37:46 +0000

    Feat: Reestructurar servicios de mensajes y conversaciones, eliminando implementaciones antiguas y creando nuevas interfaces y modelos para una mejor gestión

--- src/Domain/Interfaces/chat/IConversationService.cs
+++ src/Domain/Interfaces/chat/IConversationService.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SmileTimeNET_API.Models;
-using SmileTimeNET_API.src.Aplication.services;
+using SmileTimeNET_API.Models; 
+using SmileTimeNET_API.src.Domain.Models;
 
 namespace SmileTimeNET_API.src.Domain.Interfaces
 {
-    public interface IConversationService
+    public interface IConversationService 
     {
         Task<IEnumerable<ConversationWithLastMessage>> GetUserConversationsWithLastMessageAsync(string userId);
         Task<Conversation> GetConversationByIdAsync(int conversationId, string userId);
