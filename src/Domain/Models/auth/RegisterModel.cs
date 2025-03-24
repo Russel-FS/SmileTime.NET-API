cc67a498f7d9b7d0af4e06ebe2ab2eac182277f6
russelfloressolano900@gmail.com <russelfloressolano900@gmail.com>
2025-03-24 17:11:43 +0000

    Feat: Añadir propiedad FullName al modelo de registro para mejorar la gestión de usuarios

--- src/Domain/Models/auth/RegisterModel.cs
+++ src/Domain/Models/auth/RegisterModel.cs
@@ -8,11 +8,13 @@ namespace SmileTimeNET_API.Models
 {
     public class RegisterModel
     {
-         [Required]
+        [Required]
         [EmailAddress]
         public string? Email { get; set; }
 
         [Required]
         public string? Password { get; set; }
+
+        public string? FullName { get; set; }
     }
 }
