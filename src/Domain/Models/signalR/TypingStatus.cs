578791177c8e5b5fd155ca70d16670895f62936e
russelfloressolano900@gmail.com <russelfloressolano900@gmail.com>
2025-03-27 02:31:58 +0000

    Feat: Añadir modelo TypingStatus para gestionar el estado de escritura en conversaciones

---
+++ src/Domain/Models/signalR/TypingStatus.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmileTimeNET_API.src.Domain.Models.signalR
+{
+    public class TypingStatus
+    {
+        public string SenderId { get; set; } = string.Empty;
+        public string ReceiverId { get; set; } = string.Empty;
+        public bool IsTyping { get; set; }
+        public int? ConversationId { get; set; }
+    }
+}
